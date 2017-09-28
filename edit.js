6f61b85573f6968be85caab3ce12b084931cfcc0
Moritz Warning <moritzwarning@web.de>
2017-09-28 14:21:06 +0200

    fix node creation

--- edit.js
+++ edit.js
@@ -1,13 +1,13 @@
 
 function createEdit(graph) {
   var self = {};
+  var NODE_SPACING = 40;
 
   function genId(){
-    var hexDigits = '0123456789abcdef';
+    var chars = 'abcdefghijklmnopqrstuvwxyz';
     var id = '';
-    for (var i = 0; i < 2; i++) {
-      id += hexDigits.charAt(Math.round(Math.random() * 15));
-      id += hexDigits.charAt(Math.round(Math.random() * 15));
+    for (var i = 0; i < 4; i++) {
+      id += chars.charAt(Math.round(Math.random() * (chars.length - 1)));
     }
     return id;
   }
@@ -25,8 +25,12 @@ function createEdit(graph) {
     var nodes = [];
     var links = [];
 
+    if (count < 1 || isNaN(count)) {
+      return;
+    }
+
     for (var i = 0; i < count; i++) {
-      nodes.push({x: (i * 15), y: 0, o: {id: (id + '#' + i)}});
+      nodes.push({x: (i * NODE_SPACING), y: 0, o: {id: (id + '#' + i)}});
       if (i > 0) {
         var source = nodes[i - 1];
         var target = nodes[i];
@@ -48,12 +52,17 @@ function createEdit(graph) {
     var nodes = [];
     var links = [];
 
+    if (count < 1 || isNaN(count)) {
+      return;
+    }
+
     nodes.push({x: 0, y: 0, o: {id: (id + 0)}});
 
-    for (var i = 1; i < count; i++) {
-      var x = 20 * Math.cos(n * 2 * Math.PI/count);
-      var y = 20 * Math.sin(n * 2 * Math.PI/count);
-      nodes.push({x: x, y: y, o: {id: (id + i)}});
+    for (var i = 0; i < count; i++) {
+      var a = i * 2 * Math.PI / count;
+      var x = NODE_SPACING * Math.cos(a);
+      var y = NODE_SPACING * Math.sin(a);
+      nodes.push({x: x, y: y, o: {id: (id + (i + 1))}});
 
       var source = nodes[0];
       var target = nodes[nodes.length - 1];
@@ -68,13 +77,17 @@ function createEdit(graph) {
     var nodes = [];
     var links = [];
 
-    for (var x = 0; x < x_count; i++) {
-      for (var y = 0; y < y_count; i++) {
-        nodes.push({x: (x * 20), y: (y * 15), o: {id: (id + x + 'x' + y)}});
+    if (x_count < 1 || y_count < 1 || isNaN(x_count) || isNaN(y_count)) {
+      return;
+    }
+
+    for (var x = 0; x < x_count; x++) {
+      for (var y = 0; y < y_count; y++) {
+        nodes.push({x: (x * NODE_SPACING), y: (y * NODE_SPACING * 0.8), o: {id: (id + '_' + x + 'x' + y)}});
         if (x > 0) {
           for (var ny = 0; ny < y_count; ny++) {
-            var source = nodes[(x - 1) * ny];
-            var target = nodes[x * y];
+            var source = nodes[(x - 1) * y_count + ny];
+            var target = nodes[nodes.length - 1];
             links.push({source: source, target: target, o: {}});
           }
         }
@@ -89,24 +102,28 @@ function createEdit(graph) {
     var nodes = [];
     var links = [];
 
+    if (x_count < 1 || y_count < 1 || isNaN(x_count) || isNaN(y_count)) {
+      return;
+    }
+
     for (var x = 0; x < x_count; x++) {
       for (var y = 0; y < y_count; y++) {
-        nodes.push({x: (x * 15), y: (y * 15), o: {id: (id + '_' + x + 'x' + y)}});
+        nodes.push({x: (x * NODE_SPACING), y: (y * NODE_SPACING), o: {id: (id + '_' + x + 'x' + y)}});
       }
     }
 
     for (var x = 0; x < (x_count - 1); x++) {
       for (var y = 0; y < y_count; y++) {
-        var source = nodes[x * y];
-        var target = nodes[(x + 1) * y];
+        var source = nodes[x * y_count + y];
+        var target = nodes[(x + 1) * y_count + y];
         links.push({source: source, target: target, o: {}});
       }
     }
 
-    for (var x = 0; < x_count; x++) {
+    for (var x = 0; x < x_count; x++) {
       for (var y = 0; y < (y_count - 1); y++) {
-        var source = nodes[x * y];
-        var target = nodes[x * (y + 1)];
+        var source = nodes[x * y_count + y];
+        var target = nodes[x * y_count + (y + 1)];
         links.push({source: source, target: target, o: {}});
       }
     }
