4f2bd6acd8fc1e227acf7292c664a8abf32f44e7
Moritz Warning <moritzwarning@web.de>
2017-11-29 19:15:40 +0100

    add feature to send commands to nodes

--- src/show.js
+++ src/show.js
@@ -88,5 +88,30 @@ function createShow (graph) {
     displayElement($('show_no_items'), display);
   }
 
+  self.sendCommand = function sendCommand(cmd) {
+    var intNodes = graph.getSelectedIntNodes();
+
+    if (typeof Node.prototype.executeCommand !== "function") {
+      alert('Node.prototype.executeCommand(cmd) not implemented.');
+      return;
+    }
+
+    if (intNodes.length === 0) {
+      alert('No node selected.');
+      return;
+    }
+
+    var replies = [];
+    for (var i = 0; i < intNodes.length; i += 1) {
+      var node = intNodes[i].o;
+      var reply = node.executeCommand(cmd);
+      if(typeof ret === 'string') {
+        replies.push(reply);
+      }
+    }
+    $('command_reply').textContent = replies.join('\n');
+    graph.redraw();
+  }
+
   return self;
 }
